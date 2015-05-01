1a25f6ac466c16e616e281645d81f3482be2a98e
Rashmi Yadav <rays.rashmi@gmail.com>
2015-05-01 16:02:13 +0200

    Initial Commit

---
+++ RubyConferences/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RubyConferences
+//
+//  Created by Rashmi Yadav on 5/1/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
