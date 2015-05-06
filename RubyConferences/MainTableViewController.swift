ec99551f0c9240347432f02995f20b720d87ccda
Rashmi Yadav <rays.rashmi@gmail.com>
2015-05-06 22:51:33 +0200

    Click website url in safari

--- RubyConferences/MainTableViewController.swift
+++ RubyConferences/MainTableViewController.swift
@@ -66,10 +66,6 @@ class MainTableViewController: UITableViewController {
         
         var conferenceInfo = Conference(data: conferencesData[indexPath.row] as! NSDictionary)
         
-        
-        var yDistance = cell.frame.origin.y - 15
-        
-        var imageButton = UIButton(frame: CGRectMake(10, yDistance + 20 , 40, 40))
         var image_url = NSURL(string: conferenceInfo.image_url)
         var data = NSData(contentsOfURL: image_url!)
         var conferenceImage: UIImage? = UIImage(data: data!)
@@ -114,50 +110,12 @@ class MainTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 100.0
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) -> Void{
+        var websiteLink: String = conferencesData[indexPath.row]["website"] as! String
+        var websiteUrl: NSURL? = NSURL(string: websiteLink)
+        
+        UIApplication.sharedApplication().openURL(websiteUrl!)
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
