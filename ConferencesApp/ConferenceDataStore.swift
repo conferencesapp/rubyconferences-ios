030e26fca4a824c5f82635e30e28ebbb66759327
Rashmi Yadav <rays.rashmi@gmail.com>
2015-11-18 08:25:37 -0600

    Grouping of conferences by year and month

--- ConferencesApp/ConferenceDataStore.swift
+++ ConferencesApp/ConferenceDataStore.swift
@@ -1,6 +1,6 @@
 //
 //  ConferenceDataStore.swift
-//  
+//
 //  Created by Rashmi Yadav on 5/2/15.
 //
 //
@@ -10,13 +10,13 @@ import RealmSwift
 import Alamofire
 
 class ConferenceDataStore {
-    
+
     var conferencesData = []
-    
+
     func getConferencesFromApi() {
         let authorizationToken = "Token token=\(apiSecret)"
         let headers = ["Authorization": authorizationToken]
-        
+
         Alamofire.request(.GET, "\(apiUrl)/v3/conferences?tags=ruby", headers: headers)
             .responseJSON { (_, _, result) in
                 switch result {
@@ -24,26 +24,26 @@ class ConferenceDataStore {
                     let resultData: NSArray = JSON as! NSArray
                     self.processResults(resultData)
                     NSNotificationCenter.defaultCenter().postNotificationName("ConferencesUpdatedNotification", object: nil)
-                    
+
                 case .Failure(let data, let error):
                     print("Request failed with error: \(error)")
-                    
+
                     if let data = data {
                         print("Response data: \(NSString(data: data, encoding: NSUTF8StringEncoding)!)")
                     }
                 }
         }
     }
-    
+
     func processResults(data: NSArray) -> Void {
         self.conferencesData = data
-        
+
         let localIds: Set<Int> =  getLocalIds()
         let serverIds: Set<Int> = getServerIds()
         let removableIds = localIds.subtract(serverIds)
-        
+
         let realm = try! Realm()
-        
+
         do{
             try realm.write {
                 for(data) in self.conferencesData {
@@ -53,30 +53,30 @@ class ConferenceDataStore {
                     if let detail = data["description"] as? String {
                         conf.detail = detail
                     }
-                    
+
                     conf.location = data["location"] as! String
-                    
+
                     conf.twitter_username = self.formatTwitterUsername(data["twitter_username"] as! String)
-                    
+
                     let logos: NSDictionary = data["logos"] as! NSDictionary!
                     conf.logo_url  =  logos["thumb"] as! String!
                     conf.image_url =  logos["logo"] as! String!
-                    
+
                     conf.place = data["location"] as! String!
                     conf.when = data["when"] as! String!
-                    
+
                     conf.latitude = data["latitude"] as! Double!
                     conf.longitude = data["longitude"] as! Double!
                     conf.cfp_text = data["cfp_status"] as! String!
-                    
+
                     if let ws = data["website"] as? String{
                         conf.website = ws
                     }
-                    
+
                     conf.startDate = self.formatStartdate(data["start_date"] as! String)
                     realm.create(Conference.self, value: conf, update: true)
                 }
-            
+
             //delete expired confs
                 for(id) in removableIds{
                     let deleteConf = realm.objects(Conference).filter("id= \(id)")
@@ -87,70 +87,110 @@ class ConferenceDataStore {
             print("Error")
         }
     }
-    
+
     func formatTwitterUsername(username: String) -> String{
         var twitter: String = username
-        
+
         if twitter != "" {
             twitter =  "@\(username)"
         }
         return twitter
     }
-    
+
     func formatStartdate(stareDate: String) -> NSDate {
         let dateString = stareDate// change to your date format
-        
+
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
-        
+
         let date = dateFormatter.dateFromString(dateString)
         return date!
     }
-    
+
     func getLocalIds() -> Set<Int> {
         var localIds =  Set<Int>()
         for(lconf) in findAll() {
             localIds.insert(lconf["id"] as! Int)
         }
-        
+
         return localIds
     }
-    
+
     func getServerIds() -> Set<Int> {
         var serverIds =  Set<Int>()
         for(sconf) in self.conferencesData {
             serverIds.insert(sconf["id"] as! Int)
         }
-        
+
         return serverIds
     }
-    
+
     func findAll() -> [Conference] {
         var conferences: [Conference] = []
         for conf in try! Realm().objects(Conference).sorted("startDate") {
             conferences.append(conf)
         }
-        
+
         return conferences
     }
+
+    func groupByDate(allConferences: [Conference]) -> OrderedDictionary<String, [Conference]> {
+        var ConfGroup = OrderedDictionary<String, [Conference]>()
+
+        let conferences = allConferences
+
+        for(conference) in conferences {
+            let key = getKey(conference.startDate)
+
+            if((ConfGroup[key])?.count >= 1){
+                (ConfGroup[key])?.append(conference)
+            }
+            else{
+                ConfGroup[key] = [conference]
+            }
+
+        }
+
+        return ConfGroup
+    }
     
-    func filterConferences(searchText: String) -> [Conference]{
-        let conferences = self.findAll().filter({ (conference) -> Bool in
+    func filterConferences(searchText: String, conferences: [Conference]) -> [Conference]{
+        let tmpConferences = conferences.filter({ (conference) -> Bool in
             let tmpName: NSString = conference.name
             let tmpLocation: NSString = conference.location
+            let tmpDate: NSString = conference.when
             let rangeName = tmpName.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
             let rangeLocation = tmpLocation.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
-            return (rangeName.location != NSNotFound) || (rangeLocation.location != NSNotFound)
+            let rangeDate = tmpDate.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+
+            return (rangeName.location != NSNotFound) || (rangeLocation.location != NSNotFound) || (rangeDate.location != NSNotFound)
         })
-        
-        return conferences
+
+        return tmpConferences
     }
-    
+
     func deleteAll(){
         let realm = try! Realm()
         try! realm.write {
             realm.deleteAll()
         }
     }
-    
+
+    func getKey(date: NSDate) -> String {
+        let components = monthAndYear(date)
+
+        return "\(monthName(components.month)) \(components.year)"
+    }
+
+    func monthAndYear(date: NSDate) -> NSDateComponents {
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([.Month, .Year], fromDate: date)
+        
+        return components
+    }
+
+    func monthName(monthIndex: Int) -> String {
+        return NSDateFormatter().monthSymbols[monthIndex - 1]
+    }
+
 }
--- ConferencesApp/MainTableViewController.swift
+++ ConferencesApp/MainTableViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 import Haneke
 
-class MainTableViewController: UITableViewController, UISearchBarDelegate{
-    
+class MainTableViewController: UITableViewController, UISearchBarDelegate {
+
     @IBOutlet weak var searchBar: UISearchBar!
-    
-    let CellIdentifier = "cell"
+
     var conferences:[Conference] =  []
+    var conferencesGroup = OrderedDictionary<String, [Conference]>()
+    var filterConferencesGroup =  OrderedDictionary<String, [Conference]>()
+    let CellIdentifier = "cell"
     var filterConferences: [Conference] = []
     var conferenceDataStore = ConferenceDataStore()
     var searchActive : Bool = false
@@ -26,15 +28,16 @@ class MainTableViewController: UITableViewController, UISearchBarDelegate{
         self.refreshControl?.addTarget(self, action: "reloadData", forControlEvents: UIControlEvents.ValueChanged)
         self.tableView.addSubview(refreshControl!)
         searchBar.delegate = self
-        
+        searchBar.tintColor = UIColor(white:0.5, alpha: 1.5)
+
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "ConferencesUpdatedNotificationHandler:",
             name:"ConferencesUpdatedNotification", object: nil)
-        
+
         if Reachability().connectedToNetwork() {
             conferenceDataStore.getConferencesFromApi()
         }
     }
-    
+
     func reloadData() {
         if Reachability().connectedToNetwork() {
             conferenceDataStore.getConferencesFromApi()
@@ -43,87 +46,115 @@ class MainTableViewController: UITableViewController, UISearchBarDelegate{
             alert.show()
         }
     }
-    
+
     dynamic func ConferencesUpdatedNotificationHandler(notification: NSNotification) {
         conferences = conferenceDataStore.findAll()
-        
+        conferencesGroup = conferenceDataStore.groupByDate(conferences)
+
         self.refreshControl?.endRefreshing()
         self.tableView.reloadData()
     }
+
+    func conferenceRow(section: Int, row: Int) -> Conference {
+        let values = conferencesData()[section].1
+
+        return values[row]
+    }
     
     func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
-        searchActive = true;
+        searchActive = true
+        if(searchBar.text!.isEmpty){
+            searchActive = false
+        }
     }
-    
+
     func searchBarTextDidEndEditing(searchBar: UISearchBar) {
-        searchActive = false;
+        searchActive = false
+        self.searchBar.endEditing(true)
     }
-    
+
     func searchBarCancelButtonClicked(searchBar: UISearchBar) {
-        searchActive = false;
+        searchActive = false
+        self.searchBar.text = ""
+        self.searchBar.endEditing(true)
+        
+        self.tableView.reloadData()
     }
-    
+
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        searchActive = false;
+        searchActive = false
+        self.searchBar.endEditing(true)
+
+        self.resignFirstResponder()
     }
     
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
-        filterConferences = conferenceDataStore.filterConferences(searchText)
-        
-        if(filterConferences.count == 0){
-            searchActive = false;
-        } else {
-            searchActive = true;
-        }
-        self.tableView.reloadData()
-    }
-    
-    func conferencesData() -> [Conference]{
-        if(searchActive){
-            return filterConferences
+        if(searchText.isEmpty){
+            searchActive = false
+            searchBar.endEditing(false)
         }else{
-            return conferences
+            filterConferences = conferenceDataStore.filterConferences(searchText, conferences: conferences)
+            filterConferencesGroup = conferenceDataStore.groupByDate(filterConferences)
+
+            if(filterConferences.count == 0){
+                searchActive = false
+            } else {
+                searchActive = true
+            }
+            self.tableView.reloadData()
         }
     }
-    
+
+    func conferencesData() -> OrderedDictionary<String, [Conference]>{
+        if(searchActive){
+            return filterConferencesGroup
+        }else{
+            return conferencesGroup
+        }
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return conferencesData().array.count
     }
-    
+
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        let row_count: Int = conferencesData().count
-        
+        let row_count: Int = conferencesData()[section].1.count
+
         return row_count
     }
-    
-    
+
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let  headerCell = tableView.dequeueReusableCellWithIdentifier("headerCell") as! CustomHeaderCell
+        headerCell.headerLabel.sizeToFit()
+        headerCell.headerLabel.text = conferencesData()[section].0
+
+        return headerCell
+    }
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(CellIdentifier, forIndexPath: indexPath)
-        let conferenceInfo = conferencesData()[indexPath.row]
-        
+
+        let conferenceInfo = conferenceRow(indexPath.section, row: indexPath.row)
+
         let imageView: UIImageView = cell.contentView.viewWithTag(100) as! UIImageView
         let logo_url = NSURL(string: conferenceInfo.logo_url)!
         imageView.hnk_setImageFromURL(logo_url)
-        
+
         let title: UILabel = cell.contentView.viewWithTag(101) as! UILabel
         title.text = conferenceInfo.name
-                
+
         let location: UILabel = cell.contentView.viewWithTag(103) as! UILabel
         location.text = conferenceInfo.place
-        
+
         let when: UILabel = cell.contentView.viewWithTag(104) as! UILabel
         when.text = conferenceInfo.when
-        
+
         return cell
-    
+
     }
 
     override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
@@ -131,31 +162,38 @@ class MainTableViewController: UITableViewController, UISearchBarDelegate{
         if(self.tableView.respondsToSelector(Selector("setSeparatorInset:"))){
             self.tableView.separatorInset = UIEdgeInsetsZero
         }
-        
+
         if(self.tableView.respondsToSelector(Selector("setLayoutMargins:"))){
             self.tableView.layoutMargins = UIEdgeInsetsZero
         }
-        
+
         if(cell.respondsToSelector(Selector("setLayoutMargins:"))){
             cell.layoutMargins = UIEdgeInsetsZero
         }
-        
+
         cell.preservesSuperviewLayoutMargins = false
     }
     
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 36.0
+    }
+
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 100.0
-    }   
-    
+    }
+
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!)  {
         if segue.identifier == "viewConference" {
             let selectedRow = tableView.indexPathForSelectedRow?.row
-            let viewController = segue.destinationViewController as! ConferenceTableViewController
+            let section = tableView.indexPathForSelectedRow?.section
 
-            viewController.conference = conferences[selectedRow!]
+            let conferenceInfo = conferenceRow(section!, row: selectedRow!)
+
+            let viewController = segue.destinationViewController as! ConferenceTableViewController
+            viewController.conference = conferenceInfo
         }
     }
-    
+
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
