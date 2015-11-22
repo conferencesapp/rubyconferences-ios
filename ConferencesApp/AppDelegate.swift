08272fe2abd59b7e5d38e40f0fbc98734b09b9f4
Rashmi Yadav <rays.rashmi@gmail.com>
2015-11-22 22:27:12 +0100

    Open screen only on app is inactive or in background

--- ConferencesApp/AppDelegate.swift
+++ ConferencesApp/AppDelegate.swift
@@ -85,53 +85,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        print(userInfo["id"])
-        var id: Int = userInfo["id"] as! Int
-        print(id)
         
-        let realm = try! Realm()
-        var conference = realm.objects(Conference).filter("id=\(id)")[0]
-        var segue = UIStoryboard(name: "Main", bundle: nil)
-        var viewController = segue.instantiateViewControllerWithIdentifier("ConferenceTableViewController") as!ConferenceTableViewController
-        var rootViewController = self.window!.rootViewController as! UINavigationController
-        viewController.conference = conference
-       // rootViewController.navigationController?.popViewController(viewController, animated: true)
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
-        
-        print(conference)
-        
-        if ( application.applicationState == UIApplicationState.Inactive || application.applicationState == UIApplicationState.Background  )
-        {
+        if(application.applicationState == UIApplicationState.Background || application.applicationState == UIApplicationState.Inactive){
             
-
-        }
+            let id: Int = userInfo["id"] as! Int
+        
+            let realm = try! Realm()
+            let conference = realm.objects(Conference).filter("id=\(id)")[0]
+            let segue = UIStoryboard(name: "Main", bundle: nil)
+            let viewController = segue.instantiateViewControllerWithIdentifier("ConferenceTableViewController") as!ConferenceTableViewController
+            let rootViewController = self.window!.rootViewController as! UINavigationController
+        
+            viewController.conference = conference
+    
+            rootViewController.popToRootViewControllerAnimated(true)
+            rootViewController.pushViewController(viewController, animated: true)
+            
+      }
     }
     
-    
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
-    // MARK: - Core Data stack
     // MARK: - Core Data stack
     
     lazy var applicationDocumentsDirectory: NSURL = {
